7a64f4a10c255c6ce03690b523fc61896b789716
BoscoTU <chungchaktu@gmail.com>
2024-12-30 13:35:55 -0800

    Done spline test changed: 1, in follow trajectory run()  of MecanumDrive, added correction time == 2s(TBD)
    2, fixed the position in ticks of the two parallel dead wheels in ThreeDeadWheelLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/ThreeDeadWheelLocalizer.java
@@ -21,8 +21,9 @@ import org.firstinspires.ftc.teamcode.roadrunner.messages.ThreeDeadWheelInputsMe
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double par0YTicks = -10196.574205546232; // y position of the first parallel encoder (in tick units)
-        public double par1YTicks = 12020.981288140581; // y position of the second parallel encoder (in tick units)
+        //distance between deadwheels 12 3/8 inches 23437 ticks
+        public double par0YTicks = -11718;//-10196.574205546232; // y position of the first parallel encoder (in tick units)
+        public double par1YTicks = 11718;//12020.981288140581; // y position of the second parallel encoder (in tick units)
         public double perpXTicks = 0.0; // x position of the perpendicular encoder (in tick units)
     }
 
