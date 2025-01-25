0e8915c343f376d21e903f40b3c78ab95fb49342
Mohmed1222 <158779942+Mohmed1222@users.noreply.github.com>
2025-01-24 17:06:08 -0800

    Whatever bosco just did.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SpinnyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SpinnyTeleOp.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.SpecimenArmSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SpinningWristSubsystem;
 
 @TeleOp(name="TeleOp", group = "Real")
@@ -15,6 +16,7 @@ public class SpinnyTeleOp extends OpMode {
     ArmSubsystem armSubsystem;
     SpinningWristSubsystem spinningWristSubsystem;
     DriveSubsystem driveSubsystem;
+    SpecimenArmSubsystem specimenArmSubsystem;
 
     private boolean firstTime = true;
 
@@ -26,6 +28,7 @@ public class SpinnyTeleOp extends OpMode {
         armSubsystem = new ArmSubsystem(this);
         spinningWristSubsystem = new SpinningWristSubsystem(this);
         driveSubsystem = new DriveSubsystem(this);
+        specimenArmSubsystem = new SpecimenArmSubsystem(this);
 
         driveSubsystem.setUsingFieldCentric(false);
 
@@ -40,19 +43,19 @@ public class SpinnyTeleOp extends OpMode {
         Trigger highPosition = new Trigger(() -> gamepad2.dpad_up);
         highPosition.whenActive(() -> {
             spinningWristSubsystem.toPosition(SpinningWristSubsystem.WristPositions.OUTTAKE);
-            armSubsystem.getMoveArmToPositionCommand(ArmSubsystem.ArmPosition.HIGH_OUTTAKE_POSITION, 0.8, 0.4, 0.2).schedule();
+            armSubsystem.getMoveArmToPositionCommand(ArmSubsystem.ArmPosition.HIGH_OUTTAKE_POSITION, 0.8, 0.4, 0.25).schedule();
         });
 
         Trigger intakePosition = new Trigger(() -> gamepad2.dpad_down);
         intakePosition.whenActive(() -> {
             spinningWristSubsystem.toPosition(SpinningWristSubsystem.WristPositions.STOWED);
-            armSubsystem.getMoveArmToPositionCommand(ArmSubsystem.ArmPosition.INTAKE_POSITION, 0.8, 0.4, 0.2).schedule();
+            armSubsystem.getMoveArmToPositionCommand(ArmSubsystem.ArmPosition.INTAKE_POSITION, 0.8, 0.4, 0.25).schedule();
         });
 
         Trigger lowPosition = new Trigger(() -> gamepad2.dpad_right);
         lowPosition.whenActive(() -> {
             spinningWristSubsystem.toPosition(SpinningWristSubsystem.WristPositions.OUTTAKE);
-            armSubsystem.getMoveArmToPositionCommand(ArmSubsystem.ArmPosition.LOW_OUTTAKE_POSITION, 0.8, 0.4, 0.2).schedule();
+            armSubsystem.getMoveArmToPositionCommand(ArmSubsystem.ArmPosition.LOW_OUTTAKE_POSITION, 0.8, 0.4, 0.25).schedule();
         });
 
         Trigger linearControl = new Trigger(() -> Math.abs(gamepad2.right_stick_y) > 0);
@@ -74,8 +77,12 @@ public class SpinnyTeleOp extends OpMode {
 
         Trigger wristOuttake = new Trigger(() -> (gamepad2.b));
         wristOuttake.whenActive(() -> spinningWristSubsystem.toPosition(SpinningWristSubsystem.WristPositions.OUTTAKE));
+
         Trigger wristStorage = new Trigger(() -> (gamepad2.y));
         wristStorage.whenActive(() -> spinningWristSubsystem.toPosition(SpinningWristSubsystem.WristPositions.STOWED));
+
+        Trigger resetEncoders = new Trigger(() -> gamepad2.share);
+        resetEncoders.whenActive(() -> armSubsystem.resetEncoders());
     }
 
     private void bindDriverControls() {
@@ -91,6 +98,24 @@ public class SpinnyTeleOp extends OpMode {
         Trigger speedVariationTrigger = new Trigger(() -> gamepad1.right_trigger > 0);
         speedVariationTrigger.whileActiveContinuous(() -> driveSubsystem.setSpeedMultiplier(gamepad1.right_trigger * 0.5 + 0.5));
         speedVariationTrigger.whenInactive(() -> driveSubsystem.setSpeedMultiplier(0.5));
+
+        Trigger readyScoreSpecimen = new Trigger(() -> gamepad1.dpad_left);
+        readyScoreSpecimen.whenActive(() -> specimenArmSubsystem.readyScore());
+
+        Trigger scoreSpecimen = new Trigger(() -> gamepad1.dpad_up);
+        scoreSpecimen.whenActive(() -> specimenArmSubsystem.scoreSpecimen());
+
+        Trigger intakeSpecimen = new Trigger(() -> gamepad1.dpad_right);
+        intakeSpecimen.whenActive(() -> specimenArmSubsystem.liftPosition());
+
+        Trigger receiveSpecimen = new Trigger(() -> gamepad1.dpad_down);
+        receiveSpecimen.whenActive(() -> specimenArmSubsystem.wallPosition());
+
+        Trigger openClaw = new Trigger(() -> gamepad1.right_bumper);
+        openClaw.whenActive(() -> specimenArmSubsystem.openClaw());
+
+        Trigger closeClaw = new Trigger(() -> gamepad1.left_bumper);
+        closeClaw.whenActive(() -> specimenArmSubsystem.closeClaw());
     }
 
     @Override
